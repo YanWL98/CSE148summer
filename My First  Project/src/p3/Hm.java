d275a83c84119f6fe1e6fbc6f3d12987c4fca52f
Y WL <yanweiling29@gmail.com>
2019-05-26 19:46:37 -0400

    homework

---
+++ My First  Project/src/p3/Hm.java
@@ -0,0 +1,12 @@
+package p3;
+
+public class Hm {
+
+	public static void main(String[] args) {
+		System.out.println("\tRestaurant\tXXX\n\tAddress\t\tXXXXX\n\tTel\t\t1111111");
+		System.out.println("---------------------------------");
+		System.out.println("1. Chicken\t$10.00\n2. Beef\t\t$20.00\n3. Pork\t\t$15.00\n4. Vegetable\t$5.00\n5. Drink\t$5.00");
+
+	}
+
+}
