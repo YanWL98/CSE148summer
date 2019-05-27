7fa1956af1c3fd955bd1ef930b956fea7d9350a3
Y WL <yanweiling29@gmail.com>
2019-05-26 20:47:24 -0400

    homework

--- My First  Project/src/p3/Hm.java
+++ My First  Project/src/p3/Hm.java
@@ -13,9 +13,10 @@ public class Hm {
 		System.out.println("Please enter restaurant's Tel:");
 		String Tel = kb.nextLine();
 		System.out.println("Here's the restaurant imformation and menu");
-		System.out.println("\tRestaurant\tXXX\n\tAddress\t\tXXXXX\n\tTel\t\t1111111");
-		System.out.println("---------------------------------");
-		System.out.println("\t1. Chicken\t$10.00\n\t2. Beef\t\t$20.00\n\t3. Pork\t\t$15.00\n\t4. Vegetable\t$5.00\n\t5. Drink\t$5.00");
+		System.out.println("\tRestaurant\t" + name + "\n\tAddress\t\t" + Address + "\n\tTel\t\t" + Tel
+				+ "\n---------------------------------\n"
+				+ "\t1. Chicken\t$10.00\n\t2. Beef\t\t$20.00\n\t3. Pork\t\t$15.00\n\t4. Vegetable\t$5.00\n\t5. Drink\t$5.00");
+		
 
 	}
 
